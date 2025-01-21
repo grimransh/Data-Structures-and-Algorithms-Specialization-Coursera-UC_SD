e21af09d9fbe9514d5e5bb22273e54cf2dd4b0ef
grimransh <anuragpal572004@gmail.com>
2025-01-21 23:50:47 +0530

    Create SumofTwoDigits.cpp

---
+++ Algorithmic Toolbox/SumofTwoDigits.cpp
@@ -0,0 +1,12 @@
+// In this very first programming challenge, your goal is to implement a program that reads two digits from the standard input and prints their sum to the standard output.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 0, b = 0, sum = 0;
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum;
+    return 0;
+}
