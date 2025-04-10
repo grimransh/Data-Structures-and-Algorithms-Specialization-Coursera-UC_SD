4a6a61b3a83d889cb30ae3bcb61d408d8aff91a0
grimransh <anuragpal572004@gmail.com>
2025-04-10 15:20:42 +0530

    optimized

--- Algorithmic Toolbox/module 2/2_1_3hugeFibonacciNumber.cpp
+++ Algorithmic Toolbox/module 2/2_1_3hugeFibonacciNumber.cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int pisano(int m){
+    int a=0, b=1,c;
+    for(int i=0; i<m*m; ++i){
+        c=(a+b)%m;
+        a=b;
+        b=c;
+        if(a==0 && b==1) return i+1;
+    }
+}
 int hugeFibbNum(long long n, int m){
+    int pissa=pisano(m);
+    n%=pissa;
     if(n<=1) return n;
     int a = 0, b = 1, c;
     for(long long i=2; i<=n; i++){
